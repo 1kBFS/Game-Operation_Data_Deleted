81076517f6a46e92d6988bba0f372231e1fcdb4b
PPolosin <polosin_pi_0818@1511.ru>
2023-11-17 22:48:01 +0300

    feat: add main.cpp, add libs to CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -1,12 +1,25 @@
 #include <iostream>
-#include <memory>
-#include <vector>
-struct Test{
+#include <SFML/Graphics.hpp>
+#include <TGUI/TGUI.hpp>
+#include <TGUI/Backend/SFML-Graphics.hpp>
 
-};
 int main() {
-    std::vector<std::unique_ptr<Test>> v;
-    auto it = v.begin();
-    auto& a = *it;
-    return 0;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "[Deleted]");
+    window.setVerticalSyncEnabled(true);
+    sf::CircleShape shape(100.f);
+    shape.setFillColor(sf::Color::Green);
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        window.clear();
+        // --draw--
+        window.draw(shape);
+        window.display();
+    }
+
 }
